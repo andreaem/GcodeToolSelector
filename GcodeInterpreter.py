8caff47fa537cbf7cdd28c800cb1231d0e02f86b
Mike <andreaem@users.noreply.github.com>
2019-06-27 11:16:23 -0400

    Cleaned up a bit
    I added in ; and a few other features

--- GcodeInterpreter.py
+++ GcodeInterpreter.py
@@ -19,27 +19,32 @@ while (code):
     if code[0] == "T":
         #Switch the text to mark the file fixed
         if bool (re.search("TAILSTOCKUNFIXED", code)) :
-            ngcO ("TAILSTOCKFIXED")
+            ngcO (";TAILSTOCKFIXED")
             print ("FOUND")
         #Its a set tool command so home everyone 
-        else:
+        elif bool (re.search("^T\d\d\s.*", code)) :            
             print ("TOOL CHANGE, to number ", code[1:3]) #all tool selections are of the form T## 
             toolnum = code[1:3]            
             #Send all tool arms home and then set tool
+            ngcO ("; Adding 2 lines to home both tools before a tool change")
             ngcO ("G53 G0 X-0.125") 
             ngcO ("G53 G0 A-0.125")
             ngcO (code)
+        else:
+            print ("ERROR a faulty tool code was found")
+            ngcO ("ERROR A FAULTY TOOL CODE WAS FOUND")
+            break 
     else:
         #ngcO (code[:-1]) #-1 gets rid of the line break
     
         if int(toolnum) < toolpivot:
-            #These tools run on x and z axis
-            ngcO (code)            
-            pass
+            #These tools run on x and z axis so no changes made
+            ngcO (code)
         else :
             #these are Z only tools
             if re.search ("^G\d\d\s.X*",code):
-                print ("X motion found in z only tool, DELETED")
+                #print ("X motion found in z only tool, DELETED")
+                code = ";" + code
             else:
                 ngcO (code)
 
